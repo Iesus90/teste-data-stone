c34d2d0cf064bef2066d207d93a0cc0006bccfd4
Iesus <iesus90@gmail.com>
2024-03-05 21:54:02 -0300

    component header

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="link-container">
-    <router-link to="/form-customers">Customers</router-link>
-    <router-link to="/form-products">Products</router-link>
-    <router-link to="/association">Associations</router-link>
-  </div>
+  <Header />
   <router-view/>
 </template>
 
 <script>
-import store from './store';
-
+import Header from './components/Header/Header.vue'
+import store from './store'
 
 export default {
   name: 'App',
+  components: {
+    Header
+  },
   data() {
     return {
       customers: [],
@@ -35,35 +34,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-.link-container {
-  display: flex;
-  align-items: center;
-  justify-content: space-around;
-  border: 1px solid #ccc;
-  padding: 10px;
-  border-radius: 4px;
-  box-shadow: 0px 2px 1px -1px rgba(0,0,0,0.2), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 1px 3px 0px rgba(0,0,0,0.12);
-}
-
-.link-container a {
-  color: #2c3e50;
-  text-decoration: none;
-  padding: 8px 16px;
-  border-radius: 4px;
-}
-
-.link-container a:hover {
-  transition: 0.25s;
-  background-color: rgb(56, 130, 209);
-  color: white;
-}
+  @import './style.css';
 </style>
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="link-container">
+    <router-link to="/form-customers">Customers</router-link>
+    <router-link to="/form-products">Products</router-link>
+    <router-link to="/association">Associations</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader'
+}
+</script>
+
+<style scoped>
+  @import './style.css';
+</style>
